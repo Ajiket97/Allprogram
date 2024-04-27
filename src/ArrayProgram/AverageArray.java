07bd6be06edcccd2eaaa0d63b5233a3756a09f4d
Ajiket97 <ajitshingnare02@gmail.com>
2024-04-27 10:33:45 +0530

    my todays comment

--- src/ArrayProgram/AverageArray.java
+++ src/ArrayProgram/AverageArray.java
@@ -5,12 +5,19 @@ public class AverageArray {
 		int[] x = { 45, 65, 35, 78, 214, 569, 225, 33, 5, 4, 5, 52, 25, 55, 166, 94, 287 };
 		int avg = 0;
 		int sum = 0;
+		AverageArray a = new AverageArray();
+		a.m1();
 		for (int i = 0; i < x.length; i++) {
 			sum = sum + x[i];
 			avg = sum / x.length;
 		}
 		System.out.println(avg);
 		System.out.println(sum);
+	}
+
+	public void m1() {
+		System.out.println("program sussfully complete");
+		
 	}
 
 }
